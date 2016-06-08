63e24ba7632553cc5bbd4609cebbf4377e227c95
roman.holzner@students.fhnw.ch <roman.holzner@students.fhnw.ch>
2016-06-08 09:20:41 +0200

    Fixed touch event listener. We can slide now

--- app/src/main/java/ch/fhnw/taurus/TouchLabyrinthView.java
+++ app/src/main/java/ch/fhnw/taurus/TouchLabyrinthView.java
@@ -3,15 +3,17 @@ package ch.fhnw.taurus;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-
-import java.text.MessageFormat;
+import android.view.SurfaceHolder;
+import android.view.View;
 
 /**
  * Created by nozdormu on 03/05/2016.
  */
 public class TouchLabyrinthView extends AbstractLabyrinthView {
     private static final String LOG_TAG = TouchLabyrinthView.class.getName();
+    private GestureDetector gestureDetector;
 
     public TouchLabyrinthView(Context context) {
         super(context);
@@ -27,12 +29,15 @@ public class TouchLabyrinthView extends AbstractLabyrinthView {
 
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        Log.v(LOG_TAG,"onTouchEvent() called");
-        Log.v(LOG_TAG, MessageFormat.format("Touched at  {0}/{1}", event.getX(), event.getY()));
-        fireTouchEvent(event);
-
-        return super.onTouchEvent(event);
+    public void surfaceCreated(SurfaceHolder holder) {
+        super.surfaceCreated(holder);
+        setOnTouchListener(new OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                // ... Respond to touch events
+                Log.v(LOG_TAG,"onTouch() called");
+                fireTouchEvent(event);
+                return true;
+            }
+        });
     }
-
 }
